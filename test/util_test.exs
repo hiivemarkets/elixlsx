f89084abf37e4de59c05a5bcc02e2a7d72905bca
Kris Handley <handley.kris@gmail.com>
2024-06-18 22:06:15 +0900

    Update some deps so it compiles

--- test/util_test.exs
+++ test/util_test.exs
@@ -3,7 +3,6 @@ use PropCheck
 defmodule UtilTest do
   use ExUnit.Case, async: false
 
-  alias Elixlsx.Image
   alias Elixlsx.Util
 
   property "Util.encode_col reverses decode_col", [:verbose] do
@@ -11,7 +10,6 @@ defmodule UtilTest do
       assert Util.decode_col(Util.encode_col(x)) == x
     end
   end
-
 
   test "width_to_px" do
     assert Util.width_to_px(1, %Image{}) == 12
